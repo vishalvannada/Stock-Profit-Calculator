f4e6cf9a227717e3f730c718d2605f0a9f03a30f
vishalvannada <vishal.vannada@sjsu.edu>
2018-12-18 01:40:10 -0800

    Adding Stock Protfolio - Not Complete Yet

--- firstFlask.py
+++ firstFlask.py
@@ -4,6 +4,12 @@ from flask import Flask, jsonify, request, render_template
 import urllib3
 import datetime
 from pytz import timezone
+import json
+
+
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+
 
 app = Flask(__name__)
 
@@ -102,5 +108,100 @@ def helloStocks():
 
     return render_template('searchStocks.html');
 
+
+@app.route("/strategy", methods = ['GET','POST'])
+def helloStrategy():
+    if request.method == 'POST':
+
+        stockSymbol = request.form.get('stockSymbol');
+        strategies = request.form.getlist('strategy');
+
+        if(len(strategies) > 2):
+            return render_template('strategy.html', error = {'error' : 'Please select 1 or 2 strategies!'})
+        else:
+            with open('g.json') as f:
+                data = json.load(f)
+
+            for one in strategies:
+                topthree = data[one]
+                for ticker in topthree:
+                    print(ticker['name'])
+                    
+
+        print(strategies)
+        print(','.join(strategies))
+
+        # strategy = {
+        #     'ethical_array' : ['AAPL', 'MSFT', 'ADBE'],
+        #     'growth_array'  : ['V', 'MA', 'AXP'],
+        #     'index_array' : ['TSLA', 'F', 'HMC'],
+        #     'quality_array' : ['COST', 'WMT', 'BBY'],
+        #     'value_array' : ['FB', 'TWTR', 'GOOG']
+        # }
+        #
+        # print(strategy.items());
+        # print(strategy);
+
+        # print (data)
+
+
+
+        # ts = TimeSeries(key='C3UMGJ3EA980AWMZ', output_format='pandas')
+        # data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min')
+
+        stockSymbol = 'MSFT'
+        # result = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+stockSymbol+'&apikey=C3UMGJ3EA980AWMZ')
+
+        # print(result.json()['Meta Data'])
+        # print(data)
+        # data['close'].plot()
+        # plt.title('Intraday Times Series for the MSFT stock (1 min)')
+        # plt.show()
+
+
+
+        # try :
+        #
+        #     result = requests.get('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='+stockSymbol+'&apikey=C3UMGJ3EA980AWMZ')
+        #     data = result.json();
+        #     print('vishal', data)
+        #
+        #     symbol = data['Global Quote']['01. symbol'];
+        #     price = data['Global Quote']['05. price'];
+        #     change = data['Global Quote']['09. change'];
+        #     changePercent = data['Global Quote']['10. change percent'];
+        #
+        #     stockName = requests.get('https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords='+stockSymbol+'&apikey=C3UMGJ3EA980AWMZ')
+        #     name = stockName.json()['bestMatches'][0]['2. name']
+        #
+        #     fmt = "%a %b %d %H:%M:%S %Z %Y"
+        #     now_utc = datetime.datetime.now(timezone('US/Pacific'))
+        #     print('\nOutput : \n')
+        #
+        #     time = now_utc.strftime(fmt);
+        #
+        #
+        #     data = {
+        #     'symbol' : symbol,
+        #     'price' : price,
+        #     'change' : float(change),
+        #     'changePercent' : changePercent,
+        #     'stockName' : stockName,
+        #     'name' : name,
+        #     'time' : time
+        #     }
+        #     return render_template('searchStocks.html', data = data);
+        # except :
+        #
+        #     try:
+        #         note = data['Note'];
+        #         return render_template('searchStocks.html', error = {'error' : 'Connection Limit, Please try again later!'});
+        #     except:
+        #         return render_template('searchStocks.html', error = {'error' : 'Invalid Symbol, Please try again!'});
+
+
+
+    return render_template('strategy.html');
+
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(debug = True)
